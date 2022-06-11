f596f9a857fa79a6b047c8f43fc0961d319c3292
wangsheng <775083595@qq.com>
2022-06-11 17:04:39 +0800

    Default Changelist2

--- 车服事件/常跑路线.py
+++ 车服事件/常跑路线.py
@@ -1,32 +1,37 @@
-import requests,json
+import requests,json,hashlib
+hashlib.sha256()
+
+header={
+    "WF-Noise":"1814c327410",
+    "WF-Content-Sign": "jUihAQOJo2YlyF3kKcscV6g5ljNli4V70zF+WuGRShM=",
+    "Content-Type": "application/json;charset=utf-8",
+    "Authorization":"WF-SHA2 H-04700d5900x0002-df13049622a300x0002:25/vJqt/2yzWWRKW8T6MYZYmZ9LSbatCoM688zebtT4=",
+    "access_id":"H-04700d5900x0002-df13049622a300x0002",
+    "access_key":"5161597ac4adfbe6c5793b048fd71173668682955f6fefcd8990d359fbea1ea1"
+
+}
+url="https://api.ruixiude.com/jscf_business_route"
+data={
+    "wf_req": {
+        "ver": "1.0",
+        "trace_id": "",
+        "res_id": "",
+        "tenat_id": ""
+    },
+    "invoke": {
+        "method": "test/get_work_routes",
+        "params": {
+            "conventional_route_id": "",
+            "page": 1,
+            "vin": "LFWSRXSJ3KAD57951",
+            "_global": {}
+        }
+    }
+}
 
 
-header={"access_id":"H-0418cd7800x0001-620c041f501400x0002",
-       "Content-Type": "application/json",
-        "access_key":"0d71fe0eea05bc3af3920384395bf8a5373dafa6e2aeff5597b1dd2a7d696541"}
-url="http://wf-dev.intranet.ruixiude.com:16015/apidoc/index?name=business_route#"
-"'描述： 获取车辆最近停靠点'"
-url1=url+"route_service/get_near_area_points"
-data1={"wf_req":{"ver":"1.0","trace_id":"","res_id":"","tenat_id":""},"invoke":{"method":"route_service/get_near_area_points","params":{"count":4,"vin":"LFWSRXSJ6J1F14273","_global":{}}}}
-"'描述： 创建常跑线路'"
-url2=url+"route_service/create_route_rule"
-"'描述： 获取常跑线路'"
-url3=url+"route_service/get_route_rules"
-"'描述：  删除常跑线路'"
-url4=url+"route_service/remove_route_rule"
-"'描述： 根据常跑线路，取趟次列表'"
-url5=url+"route_service/get_routes"
-"'描述： 获取趟次详情'"
-url6=url+"route_service/get_route"
+r=requests.post(url=url,headers=header,data=json.dumps(data)).json()
+print(r)
 
 
 
-
-def add():
-    num = 0
-    while True:
-        yield num
-        num += 1
-
-a = add()
-print(next(a))
